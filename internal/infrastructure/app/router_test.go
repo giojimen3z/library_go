7ddcf35f56ed703d845f89482ddae8816805a486
giojimen3z <gioud@outlook.com>
2025-08-19 23:47:32 -0500

    feature[config]: adding fixes

--- internal/infrastructure/app/router_test.go
+++ internal/infrastructure/app/router_test.go
@@ -12,6 +12,7 @@ import (
 	mockAuthorRepo "library/internal/test/mock"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewHandlers_SetsAuthor(t *testing.T) {
@@ -22,74 +23,62 @@ func TestNewHandlers_SetsAuthor(t *testing.T) {
 
 	h := app.NewHandlers(ctrl)
 
-	if h.Author != ctrl {
-		t.Fatalf("se esperaba que Handlers.Author fuera el pasado a NewHandlers")
-	}
+	assert.Equal(t, ctrl, h.Author)
 }
 
-func TestRegisterapp_NoAuthor_POSTAuthors_Returns404(t *testing.T) {
+func TestRegisterRoutes_NoAuthor_POSTAuthors_Returns404(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
 	mockRepo := new(mockAuthorRepo.AuthorRepoMock)
 	svc := service.NewAuthorService(mockRepo)
 	useCase := application.NewAuthorApp(svc)
 	ctrl := controller.NewAuthorController(useCase)
+
 	h := app.NewHandlers(ctrl)
 	app.RegisterRoutes(r, h)
-
 	req, _ := http.NewRequest(http.MethodPost, "/api/v1/library/authors", nil)
 	rr := httptest.NewRecorder()
 	r.ServeHTTP(rr, req)
 
-	if got, want := rr.Code, http.StatusBadRequest; got != want {
-		t.Fatalf("POST /api/v1/library/authors => status %d, se esperaba %d", got, want)
-	}
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+
 }
 
-func TestRegisterapp_NoAuthor_GETAuthors_Returns404(t *testing.T) {
+func TestRegisterRoutes_NoAuthor_GETAuthors_Returns404(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
 
 	h := app.NewHandlers(nil)
 	app.RegisterRoutes(r, h)
-
 	req, _ := http.NewRequest(http.MethodGet, "/api/v1/library/authors", nil)
 	rr := httptest.NewRecorder()
 	r.ServeHTTP(rr, req)
 
-	if got, want := rr.Code, http.StatusNotFound; got != want {
-		t.Fatalf("GET /api/v1/library/authors => status %d, se esperaba %d", got, want)
-	}
+	assert.Equal(t, http.StatusNotFound, rr.Code)
 }
 
-func TestRegisterapp_NoAuthor_GETAuthorByID_Returns404(t *testing.T) {
+func TestRegisterRoutes_NoAuthor_GETAuthorByID_Returns404(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
 
 	h := app.NewHandlers(nil)
 	app.RegisterRoutes(r, h)
-
 	req, _ := http.NewRequest(http.MethodGet, "/api/v1/library/authors/123", nil)
 	rr := httptest.NewRecorder()
 	r.ServeHTTP(rr, req)
 
-	if got, want := rr.Code, http.StatusNotFound; got != want {
-		t.Fatalf("GET /api/v1/library/authors/123 => status %d, se esperaba %d", got, want)
-	}
+	assert.Equal(t, http.StatusNotFound, rr.Code)
 }
 
-func TestRegisterapp_NoAuthor_PUTAuthorByID_Returns404(t *testing.T) {
+func TestRegisterRoutes_NoAuthor_PUTAuthorByID_Returns404(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
 
 	h := app.NewHandlers(nil)
 	app.RegisterRoutes(r, h)
-
 	req, _ := http.NewRequest(http.MethodPut, "/api/v1/library/authors/123", nil)
 	rr := httptest.NewRecorder()
 	r.ServeHTTP(rr, req)
 
-	if got, want := rr.Code, http.StatusNotFound; got != want {
-		t.Fatalf("PUT /api/v1/library/authors/123 => status %d, se esperaba %d", got, want)
-	}
+	assert.Equal(t, http.StatusNotFound, rr.Code)
 }
