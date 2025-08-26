9f618aa47f74713aff98ed0f1ee2768343a6b4bb
giojimen3z <gioud@outlook.com>
2025-08-25 20:36:35 -0500

    feature[configs]:adding new models accord to  sql script

--- internal/domain/model/reservation.go
+++ internal/domain/model/reservation.go
@@ -10,14 +10,14 @@ import (
 // Schema: library.reservations
 
 type Reservation struct {
-	ID              uuid.UUID   `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	MemberID        uuid.UUID   `json:"member_id" gorm:"type:uuid;not null"`
-	Member          *Member     `json:"member,omitempty" gorm:"foreignKey:MemberID;references:ID;constraint:OnDelete:RESTRICT"`
-	BookID          uuid.UUID   `json:"book_id" gorm:"type:uuid;not null"`
-	Book            *Book       `json:"book,omitempty" gorm:"foreignKey:BookID;references:ID;constraint:OnDelete:RESTRICT"`
-	ReservedAt      time.Time   `json:"reserved_at" gorm:"autoCreateTime"`
-	CanceledAt      *time.Time  `json:"canceled_at"` // nullable
-	FulfilledLoanID *uuid.UUID  `json:"fulfilled_loan_id" gorm:"type:uuid"` // nullable
-	FulfilledLoan   *Loan       `json:"fulfilled_loan,omitempty" gorm:"foreignKey:FulfilledLoanID;references:ID;constraint:OnDelete:SET NULL"`
-	CreatedAt       time.Time   `json:"created_at" gorm:"autoCreateTime"`
+	ID              uuid.UUID  `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	MemberID        uuid.UUID  `json:"member_id" gorm:"type:uuid;not null"`
+	Member          *Member    `json:"member,omitempty" gorm:"foreignKey:MemberID;references:ID;constraint:OnDelete:RESTRICT"`
+	BookID          uuid.UUID  `json:"book_id" gorm:"type:uuid;not null"`
+	Book            *Book      `json:"book,omitempty" gorm:"foreignKey:BookID;references:ID;constraint:OnDelete:RESTRICT"`
+	ReservedAt      time.Time  `json:"reserved_at" gorm:"autoCreateTime"`
+	CanceledAt      *time.Time `json:"canceled_at"`                        // nullable
+	FulfilledLoanID *uuid.UUID `json:"fulfilled_loan_id" gorm:"type:uuid"` // nullable
+	FulfilledLoan   *Loan      `json:"fulfilled_loan,omitempty" gorm:"foreignKey:FulfilledLoanID;references:ID;constraint:OnDelete:SET NULL"`
+	CreatedAt       time.Time  `json:"created_at" gorm:"autoCreateTime"`
 }
